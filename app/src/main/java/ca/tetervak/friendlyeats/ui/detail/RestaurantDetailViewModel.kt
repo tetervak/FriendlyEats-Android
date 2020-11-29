4d19b390ca3437c651afa7c407771af51b2c8220
Alex Tetervak <tetervak@sheridancollege.ca>
2020-11-28 21:28:06 -0500

    used shorter switchMap notation

--- app/src/main/java/ca/tetervak/friendlyeats/ui/detail/RestaurantDetailViewModel.kt
+++ app/src/main/java/ca/tetervak/friendlyeats/ui/detail/RestaurantDetailViewModel.kt
@@ -1,10 +1,7 @@
 package ca.tetervak.friendlyeats.ui.detail
 
 import androidx.hilt.lifecycle.ViewModelInject
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Transformations
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
 import ca.tetervak.friendlyeats.model.Rating
 import ca.tetervak.friendlyeats.model.Restaurant
 import ca.tetervak.friendlyeats.repository.RatingRepository
@@ -18,14 +15,10 @@ class RestaurantDetailViewModel @ViewModelInject constructor(
     private val restaurantId = MutableLiveData<String>()
 
     val restaurant: LiveData<Restaurant> =
-            Transformations.switchMap(restaurantId){
-                restaurantRepository.get(it)
-            }
+            restaurantId.switchMap{ restaurantRepository.get(it) }
 
     val ratings: LiveData<List<Rating>> =
-            Transformations.switchMap(restaurantId){
-                ratingRepository.getAll(it)
-            }
+            restaurantId.switchMap{ ratingRepository.getAll(it) }
 
     fun loadData(id: String){
         restaurantId.value = id
